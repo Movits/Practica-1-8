7d3bbceb5ff4bae402a4c3d5249dfeb81a5ce4d6
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-23 11:47:21 -0300

    primer commit

---
+++ OperadoresCondicion.js
@@ -0,0 +1,16 @@
+console.log(4 < 4);
+console.log(23 == 33);
+console.log(37 >= -3);
+console.log("Joel" == "Joel");
+console.log("joel" == "Joel");
+console.log("abc" != "qwerty");
+console.log(("Joel" == "Joel") && ("abc" == "abc"));
+console.log(("Joel" == "joel") || ("abc" == "abc"));
+console.log(("joel" != "Joel") && ("abc" != "qwerty"));
+
+let EdadIngresada = 20;
+console.log((EdadIngresada >= 18) && (EdadIngresada < 100));
+console.log((EdadIngresada >= 18) || (EdadIngresada < 100));
+console.log((EdadIngresada <= 18) || (EdadIngresada < 100));
+console.log((EdadIngresada < 18) && (EdadIngresada > 100));
+console.log((EdadIngresada < 18) || (EdadIngresada > 100));
